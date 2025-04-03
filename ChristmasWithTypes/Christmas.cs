f8c75d25d4d965505f04a64cd14f912587728f96
Parker Sims <parker4al@gmail.com>
2025-04-03 14:39:21 -0500

    Added details to ChristmasWithTypes

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
+
 namespace ChristmasWithTypes
 {
     public class Christmas
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+
+        
 
     }
 }
